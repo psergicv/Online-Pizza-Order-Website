a70702b3e2edd05a9284192debd4cce45c59a430
psergicv <sv.postolachi@gmail.com>
2024-09-08 23:46:06 +0300

    First commit. Adding initial components and some HTML

---
+++ online-pizza-order/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='wrapper'>
+        <Header />
+        <div className='content'>
+          <div className='container'>
+            <div className='content__top'>
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className='content__title'>All Pizza's</h2>
+            <div className='content__items'>
+              <PizzaBlock />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ online-pizza-order/src/components/Categories.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Categories = () => {
+    return (
+        <div>
+            <ul>
+                <li class="active">All</li>
+                <li>Meat</li>
+                <li>Vegan</li>
+                <li>Grilled</li>
+                <li>Spicy</li>
+                <li>Closed</li>
+            </ul>
+        </div>
+    )
+};
+
+
+export default Categories;
