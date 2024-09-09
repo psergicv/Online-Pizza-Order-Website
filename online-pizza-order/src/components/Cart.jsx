21b7e66cd7595c89dca8ee80ba44101f0602f498
psergicv <sv.postolachi@gmail.com>
2024-09-09 21:25:55 +0300

    Adding Search and Cart comp + styles

---
+++ online-pizza-order/src/components/Cart.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../App.css";
+
+
+const Cart = () => {
+    return (
+        <div>
+            <div className="header__cart">
+                <a href="" className="button button--cart">
+                    <span>$ 0</span>
+                    <div className="button__delimiter"></div>
+                    <span>0</span>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+
+export default Cart;
